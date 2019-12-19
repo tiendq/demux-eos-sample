60ca58c74ffe505fddd8297c47ee39f59dcdab25
Tien Do <tiendq@gmail.com>
2019-12-19 15:17:09 +0700

    refactor: add dotenv configuration and log debug mode for demux

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 
 const debug = require('debug')('app');
 const express = require('express');
+const { parsed } = require('dotenv-safe').config();
 const { startWatcher } = require('./src');
+
+debug('Environment variables:\n%O', parsed);
 
 const app = express();
 
@@ -10,7 +13,7 @@ app.get('/', (request, response) => {
   response.status(200).end('Server running.');
 });
 
-app.listen(8080, () => {
+app.listen(8080, async () => {
   debug('Listening on 8080...');
 
   try {
--- src/watcher.js
+++ src/watcher.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const { BaseActionWatcher } = require('demux');
-const { NodeosActionReader } = require('demux-eos');
+const { StateHistoryPostgresActionReader } = require('demux-eos');
 const ObjectActionHandler = require('./ObjectActionHandler');
 const handler = require('./handlers/v1');
 
-const actionHandler = new ObjectActionHandler([handler]);
+const actionHandler = new ObjectActionHandler([handler], {
+  logLevel: 'debug'
+});
 
 /*
  * Since we're reading from the EOS main net, we can use the NodeosActionReader supplied by the demux-eos package. This
@@ -17,23 +19,31 @@ const actionHandler = new ObjectActionHandler([handler]);
  * More information can be found on the main demux-eos repository:
  * https://github.com/EOSIO/demux-js-eos
  */
-const actionReader = new NodeosActionReader({
+const actionReader = new StateHistoryPostgresActionReader({
   startAtBlock: -1,
   onlyIrreversible: true,
-  nodeosEndpoint: 'http://jungle2.cryptolions.io:80'
+  dbSchema: process.env.BLOCKCHAIN_DB_SCHEMA,
+  massiveConfig: {
+    host: process.env.BLOCKCHAIN_DB_HOST,
+    port: Number(process.env.BLOCKCHAIN_DB_PORT),
+    database: process.env.BLOCKCHAIN_DB_NAME,
+    user: process.env.BLOCKCHAIN_DB_USER,
+    password: process.env.BLOCKCHAIN_DB_PASSWORD
+  },
+  logLevel: 'debug'
 });
 
 /*
  * This ready-to-use base class helps coordinate the Action Reader and Action Handler, passing through block information
  * from the Reader to the Handler. The third argument is the polling loop interval in milliseconds.
  */
-const actionWatcher = new BaseActionWatcher(
-  actionReader,
-  actionHandler,
-  500
-);
+const actionWatcher = new BaseActionWatcher(actionReader, actionHandler, {
+  pollInterval: 500,
+  logLevel: 'debug'
+});
 
 async function startWatcher() {
+  await actionReader.initialize();
   await actionWatcher.watch();
 }
 
